0169313edf8791cf05d779cda35847bc4b87df4b
Yumin Chen <yumin@chen.so>
2026-07-03 20:39:34 +0100

    Fix vite.config.ts types

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,7 @@ const config = defineConfig(({ mode }) =>
 	mode === "client" ? clientConfig : mainConfig,
 );
 
-const mainConfig = {
+const mainConfig = (_mode: string) => ({
 	build: {
 		minify: "oxc" as const,
 		emptyOutDir: false,
